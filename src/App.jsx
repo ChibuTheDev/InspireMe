7cea886632dc898e9acca9bd5cb6988e4cc24dc9
Chibu <109426683+ChibuTheDev@users.noreply.github.com>
2024-02-29 16:39:32 +0100

    Revert "improving your project"

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,47 @@
-import divide from "../src/assets/pattern-divider-desktop.svg";
-import dice from "../src/assets/icon-dice.svg";
-import { useState, useEffect } from "react";
+import divide from '../src/assets/pattern-divider-desktop.svg'
+import dice from '../src/assets/icon-dice.svg'
+import { useState } from 'react'
 
 function App() {
-  const [advice, setAdvice] = useState("");
+  const [advice,setAdvice] = useState('')
+  const [adviceFetched, setAdviceFetched] = useState(false);
+  const  [id, setId] = useState(0)
 
-  function getAdvice() {
-    fetch("https://api.adviceslip.com/advice")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setAdvice(data.slip);
-      })
-      .catch((error) => {
-        alert("Error fetching advice:", error);
-      });
-  }
-  useEffect(() => {
-    getAdvice();
-  }, []);
+ function getAdvice(){
+
+  fetch('https://api.adviceslip.com/advice')
+  .then(res=>{
+   return res.json()
+  })  
+ .then(data=>{
+   setAdvice(data.slip.advice)
+   setId(data.slip.id)
+   setAdviceFetched(true)
+ })
+ .catch(error => {
+  alert('Error fetching advice:', error);
+})
+ } 
+  
+
+
   return (
     <>
-      <main className="container">
-        <section>
-          <h3>Advice #{advice.id}</h3>
-          <p>&quot;{advice.advice}&quot;</p>
-          <img className="divide" src={divide} alt="A divider" />
-        </section>
-        <div className="dice" onClick={getAdvice}>
-          <img src={dice} alt="dice to randomize advice" />
-        </div>
-      </main>
-    </>
-  );
+   <div className="container">
+     {adviceFetched?<h3>Advice #{id}</h3>:<h3>Advice #117</h3>}
+      { adviceFetched?<p>&quot;{advice}&quot;</p>:<p>It is easy to sit up and take notice, whats difficult is standing up and taking action</p>}
+  
+   <img className='divide'src={divide} alt="A divider"/>
+   <div className='dice' onClick={getAdvice}>
+    <img  src={dice} alt='dice to randomize advice'/>
+  </div>
+  
+
+  
+  
+  </div>
+ </>
+  )
 }
 
-export default App;
+export default App
